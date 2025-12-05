f71874555406440ff45575ed51bc5c6a3e9e73da
Zeynep106 <duyarzeynep104@gmail.com>
2025-12-05 20:20:23 +0300

    Add files via upload

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20, default="primary")
+
+    def _str_(self):
+        return self.name
