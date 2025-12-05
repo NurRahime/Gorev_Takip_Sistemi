c16cce285fac7f8897ac9f8ff0ff8c5f10f917b6
busra-sys <cakmakbusra481@gmail.com>
2025-12-05 18:19:26 +0300

    dosyalar eklendi

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
-def home_view(request):
-    return render(request, 'base.html')
+def home(request):
+    return render(request, 'home.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')   # varsa, yoksa oluşturabilirsin
